59e5a500cc8f0b71892830d5e9d9386f325d4bc0
Leopold-PINTARD <leopold.pintard@epitech.eu>
2025-02-11 10:27:51 +0100

    🎉 Initial commit with file structure and github actions test

---
+++ Makefile
@@ -0,0 +1,68 @@
+##
+## EPITECH PROJECT, 2024
+## Makefile
+## File description:
+## Makefile
+##
+
+.PHONY: all clean fclean re tests_run
+
+GREEN =	\033[0;32m
+YELLOW =	\033[0;33m
+RED =	\033[0;31m
+NC =	\033[0m
+
+%.o: %.cpp
+	@echo "$(YELLOW)⚙️  Compiling $(GREEN)$<$(YELLOW)$(NC)"
+	@g++ $(CPPFLAGS) -c $< -o $@
+	@echo "$(GREEN)✅ Generated $(YELLOW)$@$(GREEN) successfully$(NC)"
+
+NAME =	nanotekspice
+
+MAIN_SRC =	src/Main.cpp
+
+SRC	=	
+
+TESTS_SRC =	tests/tests.cpp
+
+OBJ =	$(SRC:.cpp=.o)
+
+MAIN_OBJ =	$(MAIN_SRC:.cpp=.o)
+
+CPPFLAGS =	-std=c++20 -Wall -Wextra -Werror -O2 -g
+
+CPPTESTFLAGS =	--coverage -lcriterion -Wall -Wextra -Werror -O2 -g
+
+all: 	$(NAME)
+
+$(NAME):	$(OBJ) $(MAIN_OBJ)
+	@echo "$(GREEN)🔨 Building $(NAME)...$(NC)"
+	@g++ -o $(NAME) $(OBJ) $(MAIN_OBJ)
+
+run: re
+	@echo "$(GREEN)🚀 Running $(NAME)...$(NC)"
+	@echo "$(YELLOW)-----------------------------------$(NC)"
+	@./$(NAME)
+
+valgrind: $(NAME)
+	@echo "$(YELLOW)🔍 Running Valgrind...$(NC)"
+	@valgrind --leak-check=full --show-leak-kinds=all ./$(NAME)
+
+tests_run:
+	@echo "$(YELLOW)🧪 Running tests...$(NC)"
+	@g++ -o unit_tests $(SRC) $(TESTS_SRC) $(CPPTESTFLAGS)
+	@./unit_tests
+	@gcovr --exclude tests/
+
+clean:
+	@echo "$(RED)🧹 Cleaning up...$(NC)"
+	@rm -f $(OBJ) $(MAIN_OBJ)
+	@rm -f *.gcda
+	@rm -f *.gcno
+
+fclean: clean
+	@echo "$(RED)🧹 Removing binaries...$(NC)"
+	@rm -f $(NAME)
+	@rm -f unit_tests
+
+re: fclean all
---
+++ tests/tests.cpp
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2025
+** repo-c++
+** File description:
+** tests
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+void redirect_all_stdout(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+std::string format_output(const std::string& expected, const std::string& actual) {
+    std::string separator = "\033[1;34m--------------------\033[0m\n";
+    std::string formatted_output = separator +
+        "\033[1;32mExpected:\033[0m\n" + expected + "\n" +
+        separator +
+        "\033[1;31mActual:\033[0m\n" + actual + "\n" +
+        separator;
+    return formatted_output;
+}
+
+Test(sample_test_suite, sample_test_case)
+{
+    int a = 5;
+    int b = 10;
+    int result = a + b;
+
+    std::string formatted_output = format_output("15", std::to_string(result));
+    cr_assert_eq(result, 15, "%s", formatted_output.c_str());
+}
+
+Test(output_test_suite, hello_world_test, .init = redirect_all_stdout)
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::string formatted_output = format_output("Hello, World!\n", "Hello, World!");
+    cr_assert_stdout_eq_str("Hello, World!\n", "%s", formatted_output.c_str());
+}
+
+Test(file_output_test_suite, file_content_test, .init = redirect_all_stdout)
+{
+    std::ifstream file("test_file.txt");
+    std::string file_content((std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>());
+    std::string expected_content = "File content to compare\n";
+
+    std::string formatted_output = format_output(expected_content, file_content);
+    cr_assert_eq(file_content, expected_content, "%s", formatted_output.c_str());
+}
