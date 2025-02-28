ee98e4898fba486abd43863aaf7c8249cfcb7df3
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-02-28 15:51:50 +0100

    ✅ Added tests for And gate input and output computations

--- tests/basicGates.cpp
+++ tests/basicGates.cpp
@@ -51,3 +51,27 @@ Test(and_tests, undefined_test)
     auto result = and_gate.compute(2);
     cr_assert_eq(result, nts::Undefined);
 }
+
+Test(and_tests, compute_input_test)
+{
+    nts::And and_gate("test");
+    class nts::True true_gate("vrai");
+    true_gate.setLink(0, and_gate, 0);
+    auto result = and_gate.compute(0);
+    auto result2 = and_gate.compute(1);
+    cr_assert_eq(result, nts::Undefined);
+    cr_assert_eq(result2, nts::Undefined);
+}
+
+Test(and_tests, compute_input_after_compute_output_test)
+{
+    nts::And and_gate("test");
+    class nts::True true_gate("vrai");
+    true_gate.setLink(0, and_gate, 0);
+    auto resultcomputer = and_gate.compute(2);
+    auto result = and_gate.compute(0);
+    auto result2 = and_gate.compute(1);
+    cr_assert_eq(resultcomputer, nts::Undefined);
+    cr_assert_eq(result, nts::True);
+    cr_assert_eq(result2, nts::Undefined);
+}
