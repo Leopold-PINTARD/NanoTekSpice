a83ebe57e31a8d3233a245193e60fa806201e00f
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-02-28 16:44:57 +0100

    📝 Added comment to Not class for info about input/output pins

--- include/components/basic/Not.hpp
+++ include/components/basic/Not.hpp
@@ -10,6 +10,7 @@
 #include "components/basic/ADoor.hpp"
 
 namespace nts {
+    // Input is pin[0], Output is pin[1]
     class Not : public ADoor {
         public:
             Not(std::string name);
