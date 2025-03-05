68507cb6ac7415e0b9e6adf89fe1e37a769833c2
Leopold PINTARD <leopold.pintard@epitech.eu>
2025-03-05 15:44:24 +0100

    ✨ Add clock special component

---
+++ src/components/special/Clock.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2025
+** NanoTekSpice
+** File description:
+** Clock
+*/
+
+#include "components/special/Clock.hpp"
+
+nts::Clock::Clock(std::string name) : Input(name)
+{
+    this->compName = name;
+}
+
+nts::Clock::~Clock()
+{
+}
+
+nts::Tristate nts::Clock::compute(size_t pin)
+{
+    (void)pin;
+    return this->pins[0].getStatus();
+}
+
+void nts::Clock::simulate(size_t tick)
+{
+    (void)tick;
+    if (this->pins[0].getStatus() == nts::Tristate::True)
+        this->pins[0].setStatus(nts::Tristate::False);
+    else if (this->pins[0].getStatus() == nts::Tristate::False)
+        this->pins[0].setStatus(nts::Tristate::True);
+    else
+        this->pins[0].setStatus(nts::Tristate::Undefined);
+}
